acfd2b7a280fe8b80a823922117162c7e902ddf8
Harini J <69135148+Harini09J@users.noreply.github.com>
2020-10-03 16:01:54 +0530

    Add files via upload

---
+++ Csharp Assessment/MVC/Program2Controller.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class Program2 : Controller
+    {
+        public ViewResult Display()
+        {
+            var context = new DatabaseEntities1();
+            var model = context.Employee.ToList();
+            return View(model);
+        }
+        public ActionResult Find(string id)
+        {
+            int EmpId = int.Parse(id);
+            var context = new DatabaseEntities1();
+            var model = context.Employee.FirstOrDefault((e) => e.EmpId == EmpId);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Find(Employee emp)
+        {
+            var context = new DatabaseEntities1();
+            var model = context.Employee.FirstOrDefault((e) => e.EmpId == emp.EmpId);
+            model.EmpName = emp.EmpName;
+            model.EmpAddress = emp.EmpAddress;
+            model.EmpSalary = emp.EmpSalary;
+            context.SaveChanges();
+            return RedirectToAction("Display");
+        }
+        public ViewResult AddEmployee()
+        {
+            var model = new Employee();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult AddEmployee(Employee emp)
+        {
+            var context = new DatabaseEntities1();
+            context.Employee.Add(emp);
+            context.SaveChanges();
+            return RedirectToAction("Display");
+        }
+        public ActionResult DeleteEmployee(string id)
+        {
+            int EmpId = int.Parse(id);
+            var context = new DatabaseEntities1();
+            var model = context.Employee.FirstOrDefault((e) => e.EmpId == EmpId);
+            context.Employee.Remove(model);
+            context.SaveChanges();
+            return RedirectToAction("Display");
+        }
+    }
+}
---
+++ Csharp Assessment/Serialization.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace CSharp
+{
+    [Serializable]
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public long Phone { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format($"The name: {Name} from {Address} is available at {Phone}");
+        }
+    }
+    class Serialization
+    {
+        static void Main(string[] args)
+        {
+            binaryExample();
+            Console.ReadKey();
+        }
+
+        private static void binaryExample()
+        {
+            Console.WriteLine("What do U want to do today: Read or Write");
+            string choice = Console.ReadLine();
+            if (choice.ToLower() == "write")
+                 serializing();
+            else
+                deserializing();
+        }
+
+        private static void deserializing()
+        {
+            FileStream fs = new FileStream("Data.bin", FileMode.Open, FileAccess.Read);
+            BinaryFormatter fm = new BinaryFormatter();
+            Student s = fm.Deserialize(fs) as Student;
+            Console.WriteLine(s.ToString());
+            fs.Close();
+        }
+
+        private static void serializing()
+        {
+            Student s = new Student();
+            s.Name = MyConsole.getString("Enter the name");
+            s.Address = MyConsole.getString("Enter the address");
+            s.Phone = MyConsole.getNumber("Enter the landline Phone no");
+            BinaryFormatter fm = new BinaryFormatter();
+            FileStream fs = new FileStream("Data.bin", FileMode.OpenOrCreate, FileAccess.Write);
+            fm.Serialize(fs, s);
+            fs.Flush();
+            fs.Close();
+        }
+    }
+}
